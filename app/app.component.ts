c8ea85ca978cbe34ab80e11309f1b84c2aa57323
luanatsainsburys <luan.au@sainsburys.co.uk>
2016-07-25 15:35:15 +0100

    Show table data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,14 @@
  import {Component} from 'angular2/core';
+ import {ProductListComponent} from './products/product-list.component';
  /**
  * name
  */
 @Component({
     selector: 'pm-app',
     template: `<div><h1>{{pageTitle}}</h1>
-        <div>My First Component</div>
-    </div>
-    `
+        <pm-products></pm-products>
+    </div>`,
+    directives: [ProductListComponent]
 })
 export class AppComponent {
     pageTitle: string = "Acme Product Management";
